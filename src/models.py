97b47dbbae26989df64fdee6a3e88c431cfba097
Neruoal <manuellareogarcia@gmail.com>
2026-04-21 17:41:47 +0000

    First

--- src/models.py
+++ src/models.py
@@ -1,8 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, Integer, ForeignKey
+from sqlalchemy import String, Integer, ForeignKey, Enum as SQLAlchemyEnum
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from enum import Enum
-from sqlalchemy import Enum as SQLAlchemyEnum
+from typing import Optional, List
 
 db = SQLAlchemy()
 
@@ -15,7 +15,7 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
 
-    favoritos: Mapped[list["Favorites"]] = relationship("Favorites", back_populates="user", cascade="all, delete-orphan")
+    favoritos: Mapped[List["Favorites"]] = relationship(back_populates="user")
 
     def serialize(self):
         return {
@@ -25,23 +25,33 @@ class User(db.Model):
 
 class Favorites(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('user.id'), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
     tipo: Mapped[TipoEnum] = mapped_column(SQLAlchemyEnum(TipoEnum), nullable=False)
-    element_id: Mapped[int] = mapped_column(Integer, nullable=False)
 
-    user: Mapped["User"] = relationship("User", back_populates="favoritos")
+    
+    planet_id: Mapped[Optional[int]] = mapped_column(ForeignKey('planet.id'))
+    character_id: Mapped[Optional[int]] = mapped_column(ForeignKey('character.id'))
+    vehicle_id: Mapped[Optional[int]] = mapped_column(ForeignKey('vehicle.id'))
+
+    
+    user: Mapped["User"] = relationship(back_populates="favoritos")
+    planet: Mapped[Optional["Planet"]] = relationship("Planet")
+    character: Mapped[Optional["Character"]] = relationship("Character")
+    vehicle: Mapped[Optional["Vehicle"]] = relationship("Vehicle")
 
     def serialize(self):
         return {
             "id": self.id,
             "user_id": self.user_id,
             "tipo": self.tipo.value,
-            "element_id": self.element_id
+            "planet_id": self.planet_id,
+            "character_id": self.character_id,
+            "vehicle_id": self.vehicle_id
         }
 
 class Vehicle(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    model: Mapped[str] = mapped_column(String(100), unique=True)
+    model: Mapped[str] = mapped_column(String(100))
     name: Mapped[str] = mapped_column(String(100), unique=True)
 
     def serialize(self):
